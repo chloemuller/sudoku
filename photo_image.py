8f296f05495d7069d1911b97ecda411c151d3aa4
sasson <cha@MacBook-Air-de-Charlotte.local>
2019-11-18 16:49:44 +0100

    ne marche pas

--- photo_image.py
+++ photo_image.py
@@ -3,7 +3,7 @@ import numpy as np
 from pylab import *
 import matplotlib.pyplot as pl
 
-fichier="Images/sudoku2.jpeg"
+fichier="Images/sudo.jpg"
 photo=Image.open(fichier)
 image=np.array(photo)
 
@@ -22,7 +22,7 @@ def transform_to_grey (image):
 
 image_gris=transform_to_grey(image)
 #imshow(image_gris)
-pl.show()
+#pl.show()
 
 carre=10
 
@@ -33,14 +33,41 @@ def moyenne_gris_image(image_gris,x,y):
             moyenne+=image_gris[i][j][0]
     return moyenne//((2*carre+1)**2)
 
+
+
 def transform_to_bw (image_gris):
-    longueur=len(image)
-    largeur=len(image[0])
-    for i in range(carre,longueur-carre):
-        for j in range (carre,largeur-carre):
-            moyenne=moyenne_gris_image(image_gris,i,j)
-            print(moyenne)
-            if moyenne>=image_gris[i][j][0]:
+    (longueur,largeur,x)=np.shape(image_gris)
+    moyenne=0
+    Moy=[[]]
+
+    for j in range (1+carre,largeur-carre+1):
+        for k in range(2*carre+1):
+            for l in range (j-carre, j+carre):
+                moyenne+=image_gris[k][l][0]//((2*carre+1)**2)
+                Moy[0]+=[moyenne]
+
+    for i in range(1+carre,longueur-carre+1):
+        #calcul de la moyenne
+        moyenne=Moy[i-carre-1][0]
+        for k in range(2*carre+1):
+            moyenne-=image_gris[i-carre][k][0]//((2*carre+1)**2)
+            moyenne+=image_gris[i+carre][k][0]//((2*carre+1)**2)
+        Moy+=[[moyenne]]
+        print(Moy)
+
+        for j in range (1+carre,largeur-carre+1):
+            #calcul de la moyenne
+            print(i,j)
+            moyenne=Moy[i-carre][j-carre-1]
+            for k in range (i-carre,i+carre):
+                moyenne-=image_gris[k][j-carre][0]//((2*carre+1)**2)
+                moyenne+=image_gris[k][j+carre][0]//((2*carre+1)**2)
+            Moy[i-carre]+=[moyenne]
+            print(Moy)
+            del(Moy[i-carre-1])
+            print(Moy)
+
+            if moyenne>image_gris[i][j][0]:
                 image_gris[i][j][0]=255
                 image_gris[i][j][1]=255
                 image_gris[i][j][2]=255
@@ -50,5 +77,5 @@ def transform_to_bw (image_gris):
                 image_gris[i][j][2]=0
     return image_gris
 
-#imshow(transform_to_bw(image_gris))
-#pl.show()
+imshow(transform_to_bw(image_gris))
+pl.show()
