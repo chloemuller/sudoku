f853e42149b362b2a78c24dbb994a4d0d90b7d3e
sasson <cha@MacBook-Air-de-Charlotte.local>
2019-11-18 11:52:02 +0100

    corrections

--- photo_image.py
+++ photo_image.py
@@ -3,7 +3,7 @@ import numpy as np
 from pylab import *
 import matplotlib.pyplot as pl
 
-fichier="Capture d’écran 2019-11-18 à 10.23.08.png"
+fichier="Images/sudoku2.jpeg"
 photo=Image.open(fichier)
 image=np.array(photo)
 
@@ -13,34 +13,33 @@ def transform_to_grey (image):
     image_gris=[[[0,0,0] for j in range (largeur)]for i in range (longueur)]
     for i in range(longueur):
         for j in range (largeur):
-            moy=(image[i][j][0]+image[i][j][1]+image[i][j][2])//3
+            moy=(int(image[i][j][0])+int(image[i][j][1])+int(image[i][j][2]))//3
+            print(moy)
             image_gris[i][j][0]=moy
             image_gris[i][j][1]=moy
             image_gris[i][j][2]=moy
     return image_gris
 
-#image_gris=transform_to_grey(image)
+image_gris=transform_to_grey(image)
 #imshow(image_gris)
-#pl.show()
+pl.show()
 
-carre=100
+carre=10
 
 def moyenne_gris_image(image_gris,x,y):
     moyenne=0
-    longueur=len(image)
-    largeur=len(image[0])
-    for i in range(x-carre,x+carre):
-        for j in range (y-carre,y+carre):
+    for i in range(x-carre,x+carre+1):
+        for j in range (y-carre,y+carre+1):
             moyenne+=image_gris[i][j][0]
-    return moyenne//(4*carre^2)
+    return moyenne//((2*carre+1)**2)
 
 def transform_to_bw (image_gris):
-    #moyenne=moyenne_gris_image(image_gris)
     longueur=len(image)
     largeur=len(image[0])
     for i in range(carre,longueur-carre):
         for j in range (carre,largeur-carre):
             moyenne=moyenne_gris_image(image_gris,i,j)
+            print(moyenne)
             if moyenne>=image_gris[i][j][0]:
                 image_gris[i][j][0]=255
                 image_gris[i][j][1]=255
@@ -51,6 +50,5 @@ def transform_to_bw (image_gris):
                 image_gris[i][j][2]=0
     return image_gris
 
-image_bw=transform_to_bw(image)
-imshow(image_bw)
-pl.show()
+#imshow(transform_to_bw(image_gris))
+#pl.show()
