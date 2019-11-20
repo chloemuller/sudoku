a80196bab72a76ce6e088a5cd122bd6f67e483c6
thomas <thomas.carril@student-cs.fr>
2019-11-20 14:32:29 +0100

    vfinale

--- contours.py
+++ contours.py
@@ -2,45 +2,28 @@ import cv2
 import copy
 from transform import *
 
-image = cv2.imread('test.png',0)
-original = copy.copy(image)
 
-# passage de l'image en blanc sur noir pour qu'elle puisse ête lue par findcontours
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#blur = cv2.medianBlur(gray, 3)
-#thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,3)
+def corners_sudoku(image):
+    # utilisation de la fonction findcontours
+    cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(cnts)==2:
+        cnts=cnts[0]
+    else :
+        cnts=cnts[1] 
+    # le mode CHAIN_APPROX_SIMPLE sort une liste de points permettant de tracer le contour on veut le contour le plus grand
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    cadre=cnts[0]
+    # on ne cheche que les quatres coins on fai donc des approximations pour les trouver ( négligeant les courbures)
+    approx=cadre
+    while len(approx)>4:
+        peri = cv2.arcLength(cadre, True)   #calcul du périmètre du cadre trouvé
+        approx = cv2.approxPolyDP(approx, 0.015 * peri, True) #approximation du polygone(le cadre du sudoku) avec un autre polygone plus simple (moins de côtés) avec un epsilon de 0.015*perimètre jusqu'à ce qu'il y ait de quatre côtés.
+    # la liste des coins est mise au bon format
+    corners=[]
+    for c in approx:
+        corners.append(list(c[0]))    
+    return corners
 
-# utilisation de la fonction findcontours
-cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-if len(cnts)==2:
-    cnts=cnts[0]
-else :
-    cnts=cnts[1] 
-
-# le mode CHAIN_APPROX_SIMPLE sort une liste de points permettant de tracer le contour on veut le contour le plus grand
-
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-cadre=cnts[0]
-
-# on ne cheche que les quatres coins on fai donc des approximations pour les trouver ( négligeant les courbures)
-approx=cadre
-while len(approx)>4:
-    peri = cv2.arcLength(cadre, True)   #calcul du périmètre du cadre trouvé
-    approx = cv2.approxPolyDP(approx, 0.015 * peri, True) #approximation des polygone(le cadre du sudoku) avec un autre polygone plus simple (moins de côté) avec un epsilon de 0.015*perimètre ce qui permet d'obtenir un polygone à quatre côté.
-
-
-
-# la liste des coins est mise au bon format
-
-corners=[]
-
-for c in approx:
-    corners.append(list(c[0]))    
-
-
-warped=four_point_transform(image,corners)
-
-cv2.namedWindow('Contours')
-cv2.imshow('Contours', warped)
-cv2.waitKey()
+def get_clean_grid(image):
+    warped=four_point_transform(image,corners_sudoku(image))
+    return warped
