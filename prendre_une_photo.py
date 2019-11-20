049057b839b88291850558e02c1851c2d1389894
Basler Tristan <tristan.basler@student-cs.fr>
2019-11-20 15:59:34 +0100

    deuxième essai video

--- prendre_une_photo.py
+++ prendre_une_photo.py
@@ -2,17 +2,17 @@ import numpy as np
 import cv2
 import time
 
-cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # video capture source camera (Here webcam of laptop)
 ret,frame = cap.read() # return a single frame in variable `frame`
 
 while(True):
-    while  not(prendre_photo()) :
-        ret,im = cap.read()
-        cv2.imshow('video test',im)
-        key = cv2.waitKey(10)
+    #while  not(prendre_photo()) :
+    ret,im = cap.read()
+    cv2.imshow('video test',im)
+    key = cv2.waitKey(10)
     cv2.imshow('img1',frame) #display the captured image
-    if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y'
-        cv2.imwrite('images/c1.png',frame)
+    if cv2.waitKey(1) :
+        cv2.imwrite('C:/Users/trisr/sudoku/grille.png',frame)
         cv2.destroyAllWindows()
         break
     if cv2.getWindowProperty("frame", 1) == -1:
@@ -20,7 +20,6 @@ while(True):
 
 cap.release()
 time.sleep(0.1)
-cap.close()
 cv2.destroyAllWindows()
 
 
