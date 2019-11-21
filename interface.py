b655acbb2c631dc7f3e3a4432a43f5a82bb9e3c5
sasson <cha@MacBook-Air-de-Charlotte.local>
2019-11-21 10:49:30 +0100

    interface d'entrée : à tester sur un autre ordi

--- interface.py
+++ interface.py
@@ -1,10 +1,80 @@
 from tkinter import *
 from tkinter.filedialog import *
+import numpy as np
+from cv2 import cv2
+import time
+import pygame
+boucle=True
 
-image=""
+image="test"
 
 def recherche_fichiers():
+    global image #important
     image = askopenfilename(filetypes=[("PNG","*.png")], title="Choisissez votre fichier")
+
+
+def photo_prise():
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_RETURN:
+            return True
+
+
+
+###PRENDRE PHOTO
+def prendre_photo():
+    pygame.init()
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # video capture source camera (Here webcam of laptop)
+    ret,frame = cap.read() # return a single frame in variable `frame`
+
+    while(True):
+        while  not photo_prise():
+            ret,im = cap.read()
+            cv2.imshow('video test',im)
+            key = cv2.waitKey(10)
+            cv2.imshow('img1',frame) #display the captured image
+            if cv2.waitKey(1) :
+                cv2.imwrite('C:/Users/trisr/sudoku/grille.png',frame) #changer le chemin
+                cv2.destroyAllWindows()
+                break
+            if cv2.getWindowProperty("frame", 1) == -1:
+                break
+
+    cap.release()
+    time.sleep(0.1)
+    cv2.destroyAllWindows()
+
+
+def change_boucle():
+    global boucle
+    boucle=False
+
+def prendre_photo2():
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # video capture source camera (Here webcam of laptop)
+    ret,frame = cap.read() # return a single frame in variable `frame`
+
+    window=Tk()
+    label=Label(text="Appuyez sur entrer pour prendre la photo")
+    label.pack()
+
+    while(True):
+        while boucle :
+            window.bind("<Return>", change_boucle)
+            ret,im = cap.read()
+            cv2.imshow('video test',im)
+            key = cv2.waitKey(10)
+            cv2.imshow('img1',frame) #display the captured image
+            if cv2.waitKey(1) :
+                cv2.imwrite('C:/Users/trisr/sudoku/grille.png',frame) #changer le chemin
+                cv2.destroyAllWindows()
+                break
+            if cv2.getWindowProperty("frame", 1) == -1:
+                break
+    window.mainloop()
+    cap.release()
+    time.sleep(0.1)
+    cv2.destroyAllWindows()
+
+
 
 def tk_interface():
     window=Tk()
@@ -12,15 +82,11 @@ def tk_interface():
     label = Label(window, text="Rentrez ici votre image de sudoku à résoudre :")
     label.pack()
 
-    #choix1=Radiobutton (window, variable=IntVar(), text="Je souhaite télécharger ma photo", value=1, command=recherche_fichiers(window))
-    #choix2=Radiobutton (window, variable=IntVar(), text="Je souhaite prendre une photo avec la webcam", value=1)
-
-    bouton_choix1 = Button(window, text="Je souhaite télécharger ma photo", activebackground="red", command=recherche_fichiers)
+    bouton_choix1 = Button(window, text="Je souhaite télécharger ma photo", activebackground="grey", command=recherche_fichiers)
     bouton_choix2 = Button(window, text="Je souhaite prendre une photo avec la webcam", command=window.quit)
     bouton_choix1.pack()
     bouton_choix2.pack()
 
     window.mainloop()
-    print(image)
 
-tk_interface()
+prendre_photo()
