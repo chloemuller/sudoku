1ebedda1fd81c0caf1ef8ba460dd334d44db23f4
Basler Tristan <tristan.basler@student-cs.fr>
2019-11-21 10:53:22 +0100

    plein d'affichage

--- prendre_une_photo.py
+++ prendre_une_photo.py
@@ -6,10 +6,10 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # video capture source camera (Here web
 ret,frame = cap.read() # return a single frame in variable `frame`
 
 while(True):
-    #while  not(prendre_photo()) :
-    ret,im = cap.read()
-    cv2.imshow('video test',im)
-    key = cv2.waitKey(10)
+    while  not(prendre_photo()) :
+        ret,im = cap.read()
+        cv2.imshow('video test',im)
+        key = cv2.waitKey(10)
     cv2.imshow('img1',frame) #display the captured image
     if cv2.waitKey(1) :
         cv2.imwrite('C:/Users/trisr/sudoku/grille.png',frame)
