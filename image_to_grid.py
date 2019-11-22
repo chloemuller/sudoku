3251d74e1d3ab9b0119537d67c349fbf7414365b
Chloé Muller <chloe.muller@student-cs.fr>
2019-11-22 10:35:05 +0100

    Nouvelle version qui prend en compte la bordure du cadre

--- image_to_grid.py
+++ image_to_grid.py
@@ -7,32 +7,37 @@ from contours import get_clean_grid
 
 
 def decoupage(nom_image, grey = True):
+    """fonction qui renvoie une liste d'images de chaque case"""
     im = cv2.imread(nom_image)
     im = get_clean_grid(im)
-    imshow(im)
-    show()
     if grey :
         grey = pngToGrey(im)
         im = greyToBin(grey)
     #on va découper la grille en 81 cases
     size = len(im)
     cell_size = size//9 #en pixels
+    border = (size%9) //2
     cells = []
     for row in range(9):
         for column in range(9):
-            cells.append(extract_cell(row, column, im, cell_size, grey))
+            cells.append(extract_cell(row, column, im, cell_size, border, grey))
     return cells
 
 
-def extract_cell(row, column, image, cell_size, grey = True):
-    left_boundary = row*cell_size
-    right_boundary = (row+1)*cell_size
-    up_boundary = column*cell_size
-    down_boundary = (column+1)*cell_size
+def extract_cell(row, column, image, cell_size, border, grey = True):
+    """fonction qui renvoie la portion d'image correspondant à la case de coordonnées (row, column)"""
+    left_boundary = row*cell_size + border
+    right_boundary = (row+1)*cell_size + border
+    up_boundary = column*cell_size + border
+    down_boundary = (column+1)*cell_size + border
     if grey :
         cell = array([[0 for i in range(cell_size)] for j in range(cell_size)])
     else :
-        cell = array([[[float(0) for c in range(image.shape[2])]for i in range(cell_size)] for j in range(cell_size)])
+        if type(image[0][0][0]) == float :
+            func = float
+        else :
+            func = int
+        cell = array([[[func(0) for c in range(image.shape[2])]for i in range(cell_size)] for j in range(cell_size)])
     for i in range(left_boundary, right_boundary):
         for j in range(up_boundary, down_boundary):
             iprime = i-left_boundary    #les indices primes representent les indices de la cellule
@@ -47,22 +52,29 @@ def extract_cell(row, column, image, cell_size, grey = True):
 
 
 def extract_amas(cell):
+    """Fonction qui renvoie la tache de pixels sombres le plus proche du centre de la case (le nombre si la case est remplie et que les conditions sont idéales)"""
+    #On part du centre de la case et on explore en cercle l'image jusqu'à trouver un pixel noir
     size_cell = len(cell)
     carte = [[-1 for i in range(size_cell)] for j in range(size_cell)]
     milieu = size_cell//2
     front = [(milieu, milieu)]
+    #La carte permet de ne pas explorer plusieurs fois les memes pixels (-1 = pixel non exploré)
     carte[milieu][milieu] = 1
     while not noir_in_front(front, cell)[0]:
         front, carte = front_superieure(front, carte, cell)
+    #On estime que le premier pixel noir que l'on rencontre appartient au nombre que l'on cherche
+    
     pixels_noirs = noir_in_front(front, cell)[1]
     amas = pixels_noirs
+    # A partir de là on se propage sur la tache noir trouvée jusqu'à la selectionner entierement (on appelle cette tache "amas")
     amas = grossir_amas(cell, amas, carte)
     return amas
 
 
 def grossir_amas(cell, amas, carte):
-    # print(cell)
-    # print(cell.shape)
+    """Fonction qui à partir d'un pixel noir renvoie la liste des pixels noirs qui y sont rattaché (la tache de pixels noirs a laquelle il appartient)"""
+    #Pour constituer l'amas on va chercher les pixels voisins de chaque pixel de l'amas
+    #Amas a grossi vérifie que l'amas a gagné au moins un pixel pendant le tour de boucle : sinon c'est qu'on a trouvé toute la tache
     amas_a_grossi = True 
     while amas_a_grossi :
         amas_a_grossi = False
@@ -81,6 +93,7 @@ def grossir_amas(cell, amas, carte):
 
 
 def noir_in_front(front, cell):
+    """vérifie s'il y a un pixel noir dans la frontiere"""
     pixels_noirs = []
     for pixel in front :
         i, j = pixel
@@ -93,6 +106,7 @@ def noir_in_front(front, cell):
 
 
 def front_superieure(front, carte, cell):
+    """calcule la frontiere supérieure"""
     nouvelle_front = []
     for pixel in front :
         for voisin in pixels_voisins(pixel):
@@ -107,13 +121,35 @@ def front_superieure(front, carte, cell):
 
 
 def pixels_voisins(pixel):
+    """renvoie les voisins direct d'un pixel"""
     i, j = pixel
     return [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]
 
+def detect_blank_cells(amas, cell_couleur):
+    """compare la couleur moyenne de l'amas de pixel (le nombre ou l'artefact de la case vide) avec la couleur moyenne de la case : si les valeurs sont proches la case ets vide, sinon on a bien détecté un nombre"""
+    # on fait la moyenne des couleurs des pixels de l'amas
+    somme = 0 
+    for pixel in amas :
+        i, j = pixel
+        for c in range(cell_couleur.shape[2]):
+            somme += cell_couleur[i][j][c]/3
+    moyenne_amas = somme/len(amas)
+    somme = 0 
+    for i in range(cell_couleur.shape[0]):
+        for j in range(cell_couleur.shape[1]):
+            for c in range(cell_couleur.shape[2]):
+                somme += cell_couleur[i][j][c]/3
+    moyenne_cell = somme/(cell_couleur.shape[0]*cell_couleur.shape[1])
+    #if case remplie
+    if moyenne_cell  - moyenne_amas > 4 :   
+        return False
+    #if case vide
+    else :
+        return True
+
 
 def amas_to_28pixels(amas):
-    print("amas", amas)
-    #On centre d'abord le nombre extrait dans le premier 
+    """fonction qui renvoie une image du nombre (= amas) extrait bien centré et de taille 28x28 pixels pour que le reseau de neurone puisse la lire correctement"""
     hauteur = max([pixel[0] for pixel in amas]) - min([pixel[0] for pixel in amas])
     largeur = max([pixel[1] for pixel in amas]) - min([pixel[1] for pixel in amas])
     espace_vide_gauche = min([pixel[0] for pixel in amas])
@@ -134,10 +170,7 @@ def amas_to_28pixels(amas):
         for j in range(cell_size):
             if (i + decallage_horizontal, j + decallage_vertical) in amas :
                 image[i][j] = 1
-    print("image", image)
     cv2_image = convert_bitmap_to_cv2(image)
-    print(cv2_image)
-    print(cv2_image.shape)
     cv2_resized_image = cv2.resize(cv2_image, (28,28))
     resized_image = convert_cv2_to_bitmap(cv2_resized_image)
     return resized_image
@@ -183,7 +216,11 @@ def prediction(images):
 
 
 def picture_to_grid(nom_image):
+    """fonction principale renvoyant la grille sous forme d'un array à partir d'une image de la grille """
     cells = decoupage(nom_image)
+    for cell in cells :
+        imshow(cell)
+        show()
     cells_couleur = decoupage(nom_image, grey = False)
     blank = []
     images = []
@@ -194,8 +231,8 @@ def picture_to_grid(nom_image):
             images.append(amas_to_28pixels(amas))
     results = prediction(array(images))
     grid = array([[0 for i in range(9)] for j in range(9)])
-    count_blank = 0
-    count_all = 0   #cette variable numérote les cases pleines
+    count_blank = 0     #cette variable numérote les cases vides
+    count_all = 0       #cette variable numérote toutes les cases
     for i in range(grid.shape[0]):
         for j in range(grid.shape[1]):
             if not blank[count_all] :
@@ -206,29 +243,10 @@ def picture_to_grid(nom_image):
 
 
 
-def detect_blank_cells(amas, cell_couleur):
-    # on fait la moyenne des couleurs des pixels de l'amas
-    somme = 0 
-    for pixel in amas :
-        i, j = pixel
-        for c in range(cell_couleur.shape[2]):
-            somme += cell_couleur[i][j][c]/3
-    moyenne_amas = somme/len(amas)
-    somme = 0 
-    for i in range(cell_couleur.shape[0]):
-        for j in range(cell_couleur.shape[1]):
-            for c in range(cell_couleur.shape[2]):
-                somme += cell_couleur[i][j][c]/3
-    moyenne_cell = somme/(cell_couleur.shape[0]*cell_couleur.shape[1])
-    #if case remplie
-    if moyenne_cell - 0.08  > moyenne_amas :    # On rajoute le -0.08 car parfois dans une case vide la couleur de l'amas est très légèrement inférieure à la couleur moyenne
-        return False
-    #if case vide
-    else :
-        return True
 
 
 
-print(picture_to_grid("sudoku_original.png"))
+
+# print(picture_to_grid("sudoku.jpg"))
 
     
